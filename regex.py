0457a68d84fcf8c3b416e8b71d0b0ac3d0fac68b
lion137 <lion137@gmail.com>
2017-01-14 00:27:34 +0000

    Create regex.py

---
+++ regex.py
@@ -0,0 +1,2 @@
+str_regex = '(https?:\/\/)?([a-z]+\d\.)?([a-z]+\.)?activeingredients\.[a-z]+(/?(work|about|contact)?/?([a-zA-z-]+)*)?/?'
+str_regex1 = '(https?:\/\/)?([a-z]+\d\.)?([a-z]+\.)?activeingredients\.[a-z]+(/?[^\s"\.]*/?)*'
